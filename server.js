2e59481a9b4f3818af4128c36d47135a8a7d5745
GuiHenry17 <gui201629@gmail.com>
2024-09-23 16:47:30 -0400

    Implementação de Rotas

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const registrado = 'Registrado com sucesso'
+
+app.get('/', (req, res) => {
+    res.send('Servidor Rodando')
+})
+
+app.get('/aula', (req, res) => {
+    res.send('Aula Acontecendo')
+})
+
+app.get('/registrar', (req, res) => {
+    res.send('Informe suas informações: <br><br> EMAIL: <input/><br><br> SENHA: <input/> <br><br> <button>Registrar</button>')
+})
+
+app.get('/login', (req, res) => {
+    res.send('Informe suas informações: <br><br> EMAIL: <input/><br><br> SENHA: <input/> <br><br> <button>Entrar</button>')
+})
+
+app.get('/admin', (req, res) => {
+    res.send('<h1>Erro:105</h1><h1>Página Administrativa (SEM ACESSO)</h1>')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor Rodando')
+})
